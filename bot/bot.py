2e53c0b28415024ed3008653fe29340be5100988
Thomas <kaulketh@gmail.com>
2022-03-22 21:19:49 +0100

    initial

---
+++ bot/bot.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------
+# bot
+# created 22.03.2022
+# Thomas Kaulke, kaulketh@gmail.com
+# https://github.com/kaulketh
+# -----------------------------------------------------------
---
+++ config/secret.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------
+# secret
+# created 22.03.2022
+# Thomas Kaulke, kaulketh@gmail.com
+# https://github.com/kaulketh
+# -----------------------------------------------------------
+
+# github
+TOKEN_GITHUB: str = None
+
+# Telegram
+ID_API_TELEGRAM: int = None
+HASH_API_TELEGRAM: str = None
+# ThK1220LedPi
+# chat IDs
+ID_CHAT_ANNI_B: int = None
+ID_CHAT_THK: int = None
+# bot
+TOKEN_TELEGRAM_BOT: str = None
+NAME_TELEGRAM_BOT: str = None
